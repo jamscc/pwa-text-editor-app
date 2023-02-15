51b451e998e903eedced446c64775cdaee63a4e0
J <jamscc22@gmail.com>
2023-02-15 11:26:46 -0500

    putDb and getDb - draft

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,35 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+
+const dbn = 'jate';
+const nm = 1;
+const id = '1';
+const rdw = 'readwrite';
+const rd = 'readonly';
+
+const putDb = async (content) => {
+  try {
+    const cn = await openDB(dbn, nm);
+    const pt = { id: id, teContent: content };
+    await cn.transaction(dbn, rdw).objectStore(dbn).put(pt);
+  } catch (re) { throw re }
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+const getDb = async () => {
+  try {
+    const cn = await openDB(dbn, nm);
+    const rt = await cn.transaction(dbn, rd).objectStore(dbn).get(id);
+    switch (true) {
+      case (!rt):
+        return;
+      default:
+        return rt.teContent;
+    };
+  } catch (re) { throw re }
+};
+
+export { putDb, getDb }
 
 initdb();
